cc0bbeaf2aee2cbc8c830f831a9dbdb533a0d047
Pak Au <pakchungau@gmail.com>
2017-07-30 17:00:03 +1000

    customize material theme

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import injectTapEventPlugin from "react-tap-event-plugin";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import Drawer from "material-ui/Drawer";
 import MenuItem from "material-ui/MenuItem";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
 injectTapEventPlugin();
 
 class App extends Component {
@@ -23,23 +24,21 @@ class App extends Component {
 
   render() {
     return (
-      <MuiThemeProvider>
-        <div className="App">
-          <AppBar
-            title="Title"
-            iconClassNameRight="muidocs-icon-navigation-expand-more"
-            onLeftIconButtonTouchTap={this.opendraw}
-          />
-          <Drawer
-            open={this.state.open}
-            docked={false}
-            onRequestChange={open => this.setState({ open })}
-          >
-            <MenuItem>Menu Item</MenuItem>
-            <MenuItem>Menu Item 2</MenuItem>
-          </Drawer>
-        </div>
-      </MuiThemeProvider>
+      <div className="App">
+        <AppBar
+          title="Title"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+          onLeftIconButtonTouchTap={this.opendraw}
+        />
+        <Drawer
+          open={this.state.open}
+          docked={false}
+          onRequestChange={open => this.setState({ open })}
+        >
+          <MenuItem>Menu Item</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+        </Drawer>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { cyan500 } from "material-ui/styles/colors";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: "#21412a"
+  },
+  appBar: {
+    height: 50
+  }
+});
+
+ReactDOM.render(
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById("root")
+);
 registerServiceWorker();

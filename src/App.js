be62255695e787e35c26f40657f3cf05094fa472
Pak Au <pakchungau@gmail.com>
2017-07-30 15:06:05 +1000

    added drawer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AppBar from "material-ui/AppBar";
+import injectTapEventPlugin from "react-tap-event-plugin";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import Drawer from "material-ui/Drawer";
+import MenuItem from "material-ui/MenuItem";
+injectTapEventPlugin();
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+    this.opendraw = this.opendraw.bind(this);
+  }
+
+  opendraw(event) {
+    this.setState({
+      open: !this.state.open
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <MuiThemeProvider>
+        <div className="App">
+          <AppBar
+            title="Title"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+            onLeftIconButtonTouchTap={this.opendraw}
+          />
+          <Drawer
+            open={this.state.open}
+            docked={false}
+            onRequestChange={open => this.setState({ open })}
+          >
+            <MenuItem>Menu Item</MenuItem>
+            <MenuItem>Menu Item 2</MenuItem>
+          </Drawer>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
